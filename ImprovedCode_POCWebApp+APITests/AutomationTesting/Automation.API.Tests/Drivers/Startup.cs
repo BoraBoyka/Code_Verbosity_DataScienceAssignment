f8c215ee26908d064c40180aeee5518fbc7b238c
Chetan Bora <107429077+BoraBoyka@users.noreply.github.com>
2023-04-30 23:00:04 -0400

    Create Startup.cs

---
+++ ImprovedCode_POCWebApp+APITests/AutomationTesting/Automation.API.Tests/Drivers/Startup.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace AutomationTesting.Driver
+{
+    public class Startup
+    {
+        public string BrowserType { get; set; }
+        public string OS { get; set; }
+        public string ApplicationENV_URL { get; set; }
+        public string ChromeVersion { get; set; }
+        public string EdgeVersion { get; set; }
+        public string ConnectionString { get; set; }
+        public string ENV { get; set; }
+    }
+}
